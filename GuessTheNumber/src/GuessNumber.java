ef2695c26fff122e1ae64507709cbab7cd56bf08
Kaymon McCain <kaymonCodeM@outlook.com>
2022-07-27 12:27:08 -0500

    Added Try/catch to all user inputs

--- GuessTheNumber/src/GuessNumber.java
+++ GuessTheNumber/src/GuessNumber.java
@@ -7,36 +7,60 @@ public class GuessNumber {
         int num = rand.nextInt(20)+1;
         int guesses = 0;
         int tries = 6;
+
         Scanner userInput = new Scanner(System.in);
 
         System.out.println("Hello! What is your name?\n");
-        String userName = userInput.next();
+        String userName = "";
 
-        System.out.println("\nWell, "+ userName +", I am thinking of a number between 1 and 20.");
+        try{
+            userName += userInput.next();
 
-        while (guesses<=tries) {
+            System.out.println("\nWell, "+ userName +", I am thinking of a number between 1 and 20.");
+        }catch (Exception e){
+            System.out.println("\nUser name is invalid");
+        }
+
+
+        while (guesses<=tries && !userName.isEmpty()) {
             System.out.println("Take a guess.\n");
 
             String userGussString = userInput.next();
-            int userGuessNumber = Integer.parseInt(userGussString);
-            guesses++;
+            int userGuessNumber;
 
-            if (num == userGuessNumber) {
-                System.out.println("Good job, " + userName + "! You guessed my number in " + guesses + " guesses!");
-                System.out.println("Would you like to play again? (y or n)\n");
-                String again = userInput.next();
+            try {
+                userGuessNumber = Integer.parseInt(userGussString);
 
-                if ('y' == again.charAt(0)) {
-                    guesses = 0;
-                    num = rand.nextInt(20) + 1;
+                if (num == userGuessNumber) {
+                    System.out.println("Good job, " + userName + "! You guessed my number in " + guesses + " guesses!");
+                    System.out.println("Would you like to play again? (y or n)\n");
+                    String again;
+
+                    try {
+                        again = userInput.next();
+
+                        if ('y' == again.charAt(0)) {
+                            guesses = 0;
+                            num = rand.nextInt(20) + 1;
+                        } else {
+                            break;
+                        }
+
+                    }catch (Exception e){
+                        System.out.println("Very bad input. Good Bye and Good Day");
+                        break;
+                    }
+
+                } else if (num<userGuessNumber) {
+                    System.out.println("Your guess is too high.");
                 } else {
-                    break;
+                    System.out.println("Your guess is too low.");
                 }
-            } else if (num<userGuessNumber) {
-                System.out.println("Your guess is too high.");
-            } else if (num>userGuessNumber) {
-                System.out.println("Your guess is too low.");
+
+            }catch (Exception e){
+                System.out.println("Guessed number is invalid... That counts as a guess. You have " + (tries-(guesses+1)) + " guesses left");
             }
+            guesses++;
         }
 
         if (guesses>tries){
